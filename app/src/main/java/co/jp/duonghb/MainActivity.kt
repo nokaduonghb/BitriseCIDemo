d6fb60e5a46a2fd6f3608445ca61132a1d2e33a9
nokaduonghb <duong.hoang@nokasoft.com>
2022-05-06 15:12:42 +0700

    check ktlint

--- app/src/main/java/co/jp/duonghb/MainActivity.kt
+++ app/src/main/java/co/jp/duonghb/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+      Text(text = "Hello $name!")
 }
 
 @Preview(showBackground = true)
