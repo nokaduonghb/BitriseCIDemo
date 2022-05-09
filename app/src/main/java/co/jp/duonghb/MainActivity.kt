7bc3f8787be70986311f508c3b9ef031410c9558
nokaduonghb <duong.hoang@nokasoft.com>
2022-05-09 11:18:00 +0700

    check ktlint

--- app/src/main/java/co/jp/duonghb/MainActivity.kt
+++ app/src/main/java/co/jp/duonghb/MainActivity.kt
@@ -32,13 +32,13 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+      Text(text = "Hello $name!")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    BitriseCIDemoTheme {
+      BitriseCIDemoTheme {
         Greeting("Android")
     }
 }
